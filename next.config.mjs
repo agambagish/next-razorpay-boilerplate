6cd8cb7cf9cb7d21f1dd111d186b89c3310b3f9d
AkashDotJS <akash06082006@gmail.com>
2025-05-14 22:20:34 +0530

    :key: t3 env setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import { createJiti } from "jiti";
+import { fileURLToPath } from "node:url";
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+jiti("./src/env");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
---
+++ src/env.ts
@@ -0,0 +1,29 @@
+/* eslint-disable node/no-process-env */
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    RAZORPAY_KEY_ID: z.string(),
+    RAZORPAY_KEY_SECRET: z.string(),
+  },
+  runtimeEnv: {
+    RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
+    RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET,
+  },
+  emptyStringAsUndefined: true,
+  onValidationError: (issues) => {
+    console.error(
+      "❌ Invalid env variables:",
+      JSON.stringify(issues.map(
+        i => ({ [i.path?.[0] as string]: i.message.toLowerCase() }),
+      ), null, 2),
+    );
+    process.exit(1);
+  },
+  onInvalidAccess: () => {
+    throw new Error(
+      "❌ Attempted to access a server-side environment variable on the client",
+    );
+  },
+});
